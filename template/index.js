c7c69fcc2ee677284d5756c5afa6f97bb49c898d
Kyrylo Hrecheniuk a.k.a Zhytomyr Wild <kyryloh@wix.com>
2024-12-04 09:31:15 +0100

    update template

--- template/index.js
+++ template/index.js
@@ -5,21 +5,20 @@ const NAME = `\n\n--- Day ${DAY}: TODO ---`
 
 /**
  * @param input {string}
+ * @param part {number}
  * @returns {number}
  */
-function main(input) {
+function main(input, part) {
 
-  return input.length
+  return input.length + part
 }
 
 console.log(NAME)
 getInput(DAY)
   .then(input => {
-    console.log('part1:')
-    const part1Result = main(input)
-    console.log('answer:', part1Result)
+    const part1Result = main(input, 1)
+    console.log('p1:', part1Result)
 
-    console.log('\npart2:')
-    const part2Result = main(input)
-    console.log('answer:', part2Result)
+    const part2Result = main(input, 2)
+    console.log('p2:', part2Result)
   })
