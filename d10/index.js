27db90621bba710a0de9f44a8bb3904d1957382a
Kyrylo Hrecheniuk a.k.a Zhytomyr Wild <kyryloh@wix.com>
2024-12-10 18:19:30 +0100

    d10: part2

--- d10/index.js
+++ d10/index.js
@@ -14,40 +14,75 @@ const NAME = `\n\n--- Day ${DAY}: Hoof It ---`;
  * @param path {number[][]}
  * @param paths {Map<string,number[][]>}
  */
-function traverseMap(map, [y, x], path, paths) {
+function traverseMap1(map, [y, x], path, paths) {
   if (map[y][x] === 9) {
-    paths.set([...path[0], y, x].join(), path)
-    return
+    paths.set([...path[0], y, x].join(), path);
+    return;
   }
 
   let top = [y - 1, x];
   if (isInside(map, top) && map[top[0]][top[1]] - map[y][x] === 1) {
-    traverseMap(map, top, [...path, top], paths);
+    traverseMap1(map, top, [...path, top], paths);
   }
 
   let left = [y, x - 1];
   if (isInside(map, left) && map[left[0]][left[1]] - map[y][x] === 1) {
-    traverseMap(map, left, [...path, left], paths);
+    traverseMap1(map, left, [...path, left], paths);
   }
 
   let bottom = [y + 1, x];
   if (isInside(map, bottom) && map[bottom[0]][bottom[1]] - map[y][x] === 1) {
-    traverseMap(map, bottom, [...path, bottom], paths);
+    traverseMap1(map, bottom, [...path, bottom], paths);
   }
 
   let right = [y, x + 1];
   if (isInside(map, right) && map[right[0]][right[1]] - map[y][x] === 1) {
-    traverseMap(map, right, [...path, right], paths);
+    traverseMap1(map, right, [...path, right], paths);
   }
 }
 
 /**
  * @param map {number[][]}
+ * @param current {number[]}
+ * @param path {number[][]}
+ * @param paths {Map<string,number[][]>}
+ */
+function traverseMap2(map, [y, x], path, paths) {
+  if (map[y][x] === 9) {
+    paths.set([...path.map(p => p.join())].join('|'), path);
+    return;
+  }
+
+  let top = [y - 1, x];
+  if (isInside(map, top) && map[top[0]][top[1]] - map[y][x] === 1) {
+    traverseMap2(map, top, [...path, top], paths);
+  }
+
+  let left = [y, x - 1];
+  if (isInside(map, left) && map[left[0]][left[1]] - map[y][x] === 1) {
+    traverseMap2(map, left, [...path, left], paths);
+  }
+
+  let bottom = [y + 1, x];
+  if (isInside(map, bottom) && map[bottom[0]][bottom[1]] - map[y][x] === 1) {
+    traverseMap2(map, bottom, [...path, bottom], paths);
+  }
+
+  let right = [y, x + 1];
+  if (isInside(map, right) && map[right[0]][right[1]] - map[y][x] === 1) {
+    traverseMap2(map, right, [...path, right], paths);
+  }
+}
+
+/**
+ * @param map {number[][]}
+ * @param n {number}
  * @returns {number}
  */
-function part1(map) {
+function partN(map, n) {
   let starts = findNbr(map, 0);
-  let paths = new Map()
+  let paths = new Map();
+  let traverseMap = n === 1 ? traverseMap1 : traverseMap2;
   for (let i = 0; i < starts.length; i++) {
     traverseMap(map, starts[i], [starts[i]], paths);
   }
@@ -62,29 +97,8 @@ function part1(map) {
  */
 function main(input, part) {
   let map = parseInputToMap(input).map(row => row.map(it => it === '.' ? -1 : parseInt(it)));
-
-  switch (part) {
-    case 1:
-      return part1(map);
-    case 2:
-      return part;
-    default:
-      throw new Error(`Only 2 parts. There is no part ${part}`);
-  }
+  return partN(map, part);
 }
-
-// let example = `
-// ..90..9
-// ...1.98
-// ...2..7
-// 6543456
-// 765.987
-// 876....
-// 987....
-// `;
-// let result = main(example, 1);
-// console.log(result);
-
 
 console.log(NAME);
 getInput(DAY)
