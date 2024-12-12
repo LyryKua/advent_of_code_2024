52d69dce0455e7f45b679020d80def1e97a6ff0a
Kyrylo Hrecheniuk a.k.a Zhytomyr Wild <kyryloh@wix.com>
2024-12-12 20:13:00 +0100

    d12: part1: count area and perimeter

--- d12/index.js
+++ d12/index.js
@@ -15,32 +15,47 @@ const NAME = `\n\n--- Day ${DAY}: Garden Groups ---`;
 /**
  * @param map {string[][]}
  * @param start {number[]}
- * @param origin {string}
- * @param visited {number[][]}
+ * @param originPosition {number[]}
+ * @param hash {Map<string, {coordinates: number[][], area: number, perimeter: number}>}
  */
-function traceMap(map, start, origin, visited) {
+function traceMap(map, start, originPosition, hash) {
+  let origin = map[originPosition[0]][originPosition[1]];
+  let visited = hash.get(originPosition.join());
+
   let left = addVectors(start, [0, -1]);
-  if (isInside(map, left) && !isArrayInclude(visited, left) && isElemEqual(map, left, origin)) {
-    visited.push(left);
-    traceMap(map, left, origin, visited);
+  if (isInside(map, left) && !isArrayInclude(visited.coordinates, left) && isElemEqual(map, left, origin)) {
+    visited.coordinates.push(left);
+    visited.area += 1;
+    traceMap(map, left, originPosition, hash);
+  } else {
+    visited.perimeter += isArrayInclude(visited.coordinates, left) ? 0 : 1;
   }
 
   let right = addVectors(start, [0, 1]);
-  if (isInside(map, right) && !isArrayInclude(visited, right) && isElemEqual(map, right, origin)) {
-    visited.push(right);
-    traceMap(map, right, origin, visited);
+  if (isInside(map, right) && !isArrayInclude(visited.coordinates, right) && isElemEqual(map, right, origin)) {
+    visited.coordinates.push(right);
+    visited.area += 1;
+    traceMap(map, right, originPosition, hash);
+  } else {
+    visited.perimeter += isArrayInclude(visited.coordinates, right) ? 0 : 1;
   }
 
   let top = addVectors(start, [-1, 0]);
-  if (isInside(map, left) && !isArrayInclude(visited, top) && isElemEqual(map, top, origin)) {
-    visited.push(top);
-    traceMap(map, top, origin, visited);
+  if (isInside(map, left) && !isArrayInclude(visited.coordinates, top) && isElemEqual(map, top, origin)) {
+    visited.coordinates.push(top);
+    visited.area += 1;
+    traceMap(map, top, originPosition, hash);
+  } else {
+    visited.perimeter += isArrayInclude(visited.coordinates, top) ? 0 : 1;
   }
 
   let bottom = addVectors(start, [1, 0]);
-  if (isInside(map, left) && !isArrayInclude(visited, bottom) && isElemEqual(map, bottom, origin)) {
-    visited.push(bottom);
-    traceMap(map, bottom, origin, visited);
+  if (isInside(map, left) && !isArrayInclude(visited.coordinates, bottom) && isElemEqual(map, bottom, origin)) {
+    visited.coordinates.push(bottom);
+    visited.area += 1;
+    traceMap(map, bottom, originPosition, hash);
+  } else {
+    visited.perimeter += isArrayInclude(visited.coordinates, bottom) ? 0 : 1;
   }
 }
 
@@ -59,13 +74,14 @@ function part1(map) {
  */
 function main(input, part) {
   let map = parseInputToMap(input);
-  let start = [0, 0];
-  let origin = map[start[0]][start[1]];
-  /** @type {number[][]} */
-  let visited = [start];
+  let start = [1, 2];
+  let origin = [...start];
+  /** @type {Map<string, {coordinates: number[][], area: number, perimeter: number}>} */
+  let visited = new Map();
+  visited.set(start.join(), { coordinates: [start], area: 1, perimeter: 0 });
 
   traceMap(map, start, origin, visited);
-  console.log(visited);
+  console.log(visited.get(origin.join()));
   printMap(map);
   // switch (part) {
   //   case 1:
@@ -78,10 +94,10 @@ function main(input, part) {
 }
 
 let example = `
-AAAAAAA
-BBCDAAA
-BBAAAAE
-EEECAAA
+AAAA
+BBCD
+BBCC
+EEEC
 `;
 let result = main(example, 1);
 console.log('result:', result);
