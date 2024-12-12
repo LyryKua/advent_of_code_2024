2a4212563fcb2d3552b75797221e4cdc48d99083
Kyrylo Hrecheniuk a.k.a Zhytomyr Wild <kyryloh@wix.com>
2024-12-12 09:04:45 +0100

    d11: part1: refactor

--- d11/index.js
+++ d11/index.js
@@ -22,34 +22,35 @@ function splitNbr(nbr) {
 
 /**
  * @param stone {number}
- * @param map {Map<number, number>}
+ * @returns {number[]}
+ */
+function blink(stone) {
+  if (stone === 0) {
+    return [1];
+  } else if (countNbrLen(stone) % 2 === 0) {
+    return splitNbr(stone);
+  } else {
+    return [stone * 2024];
+  }
+}
+
+/**
+ * @param stones {number[]}
  * @param iterations {number}
+ * @param map {Map<string, number>}
  * @returns {number}
  */
-function calcLen(stone, map, iterations) {
-  let savedLen = map.get(stone);
-  if (savedLen === undefined) {
-    if (stone === 0) {
-      savedLen = 1;
-    } else if (countNbrLen(stone) % 2 === 0) {
-      savedLen = 2;
-    } else {
-      savedLen = 1;
-    }
-    map.set(stone, savedLen);
+function calcLen(stones, iterations, map) {
+  if (iterations === 0) {
+    return stones.length;
   }
-  if (iterations === 1) {
-    return savedLen;
+  let calculatedLen = 0;
+  for (let i = 0; i < stones.length; i++) {
+    let newStones = blink(stones[i]);
+    calculatedLen += calcLen(newStones, iterations - 1, map);
   }
-  let len = 0;
-  if (savedLen === 1) {
-    len += calcLen(stone === 0 ? 1 : stone * 2024, map, iterations - 1);
-  } else {
-    let split = splitNbr(stone);
-    len += calcLen(split[0], map, iterations - 1);
-    len += calcLen(split[1], map, iterations - 1);
-  }
-  return len;
+
+  return calculatedLen;
 }
 
 /**
@@ -58,12 +59,11 @@ function calcLen(stone, map, iterations) {
  * @returns {number}
  */
 function part(stones, iterations) {
-  /** @type {Map<number, number>} */
+  /** @type {Map<string, number>} */
   let map = new Map();
-  let newStones = stones;
   let newLen = 0;
-  for (let i = 0; i < newStones.length; i++) {
-    newLen += calcLen(newStones[i], map, iterations);
+  for (let i = 0; i < stones.length; i++) {
+    newLen += calcLen([stones[i]], iterations, map);
   }
 
   return newLen;
@@ -79,20 +79,20 @@ function main(input, n) {
   return part(arr, n === 1 ? 25 : 75);
 }
 
-// let example = '0';
+let example = '0';
 // let example = '125 17';
 // let example = '253000 1 7'
 // let example = '512 72 2024 2 0 2 4 2867 6032'
 // let example = '4 0 4 8 20 24 4 0 4 8 8 0 9 6'
-// let result = main(example, 1);
-// console.log(result);
+let result = main(example, 1);
+console.log(result);
 
-console.log(NAME);
-getInput(DAY)
-  .then(input => {
-    const part1Result = main(input, 1);
-    console.log('p1:', part1Result);
-
-    const part2Result = main(input, 2);
-    console.log('p2:', part2Result);
-  });
+// console.log(NAME);
+// getInput(DAY)
+//   .then(input => {
+//     const part1Result = main(input, 1);
+//     console.log('p1:', part1Result);
+//
+//     const part2Result = main(input, 2);
+//     console.log('p2:', part2Result);
+//   });
