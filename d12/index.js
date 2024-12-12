ddccf40cf4d54be148facccfb587aa95ca3770f7
Kyrylo Hrecheniuk a.k.a Zhytomyr Wild <kyryloh@wix.com>
2024-12-12 19:52:21 +0100

    d12: part1: bfs/dfs

--- d12/index.js
+++ d12/index.js
@@ -1,7 +1,56 @@
-import { getInput, parseInputToMap } from '../lib/index.js';
+import {
+  addVectors,
+  getInput,
+  isArrayInclude,
+  isElemEqual,
+  isInside,
+  parseInputToMap,
+  printMap,
+  putChrInMap,
+} from '../lib/index.js';
 
 const DAY = 12;
 const NAME = `\n\n--- Day ${DAY}: Garden Groups ---`;
+
+/**
+ * @param map {string[][]}
+ * @param start {number[]}
+ * @param origin {string}
+ * @param visited {number[][]}
+ */
+function traceMap(map, start, origin, visited) {
+  let left = addVectors(start, [0, -1]);
+  if (isInside(map, left) && !isArrayInclude(visited, left) && isElemEqual(map, left, origin)) {
+    visited.push(left);
+    traceMap(map, left, origin, visited);
+  }
+
+  let right = addVectors(start, [0, 1]);
+  if (isInside(map, right) && !isArrayInclude(visited, right) && isElemEqual(map, right, origin)) {
+    visited.push(right);
+    traceMap(map, right, origin, visited);
+  }
+
+  let top = addVectors(start, [-1, 0]);
+  if (isInside(map, left) && !isArrayInclude(visited, top) && isElemEqual(map, top, origin)) {
+    visited.push(top);
+    traceMap(map, top, origin, visited);
+  }
+
+  let bottom = addVectors(start, [1, 0]);
+  if (isInside(map, left) && !isArrayInclude(visited, bottom) && isElemEqual(map, bottom, origin)) {
+    visited.push(bottom);
+    traceMap(map, bottom, origin, visited);
+  }
+}
+
+/**
+ * @param map {string[][]}
+ * @returns {number}
+ */
+function part1(map) {
+  return map.length;
+}
 
 /**
  * @param input {string}
@@ -9,26 +58,33 @@ const NAME = `\n\n--- Day ${DAY}: Garden Groups ---`;
  * @returns {number}
  */
 function main(input, part) {
-  let map = parseInputToMap(input)
+  let map = parseInputToMap(input);
+  let start = [0, 0];
+  let origin = map[start[0]][start[1]];
+  /** @type {number[][]} */
+  let visited = [start];
 
-  switch (part) {
-    case 1:
-      return part;
-    case 2:
-      return part;
-    default:
-      throw new Error(`Only 2 parts. There is no part ${part}`);
-  }
+  traceMap(map, start, origin, visited);
+  console.log(visited);
+  printMap(map);
+  // switch (part) {
+  //   case 1:
+  //     return part1(map);
+  //   case 2:
+  //     return part;
+  //   default:
+  //     throw new Error(`Only 2 parts. There is no part ${part}`);
+  // }
 }
 
 let example = `
-AAAA
-BBCD
-BBCC
-EEEC
+AAAAAAA
+BBCDAAA
+BBAAAAE
+EEECAAA
 `;
 let result = main(example, 1);
-console.log(result);
+console.log('result:', result);
 
 // console.log(NAME)
 // getInput(DAY)
