66877862fcabed3be0579febfbda9cdeb82a938d
Kyrylo Hrecheniuk a.k.a Zhytomyr Wild <kyryloh@wix.com>
2024-12-16 11:33:28 +0100

    d16: dfs

--- d16/index.js
+++ d16/index.js
@@ -1,7 +1,39 @@
-import { findChar, getInput, parseInputToMap } from '../lib/index.js';
+import {
+  addVectors, duplicateMap,
+  findChar,
+  getInput,
+  getNeighbors, isArrayInclude,
+  isArraysEqual,
+  isInside,
+  parseInputToMap, printMap, putChrInMap,
+} from '../lib/index.js';
 
 const DAY = 16;
 const NAME = `\n\n--- Day ${DAY}: Reindeer Maze ---`;
+
+/**
+ * @param maze {string[][]}
+ * @param start {number[]}
+ * @param end {number[]}
+ * @param visited {number[][]}
+ * @param paths {number[][][]}
+ * @return {number[][][]}
+ */
+function dfsMaze(maze, start, end, visited, paths) {
+  if (!isInside(maze, start) || isArraysEqual(start, end)) {
+    paths.push(visited);
+    return paths;
+  }
+  let neighbors = getNeighbors(maze, start);
+  for (let i = 0; i < neighbors.length; i++) {
+    if (maze[neighbors[i][0]][neighbors[i][1]] !== '#' && !isArrayInclude(visited, neighbors[i])) {
+      visited = [...visited, neighbors[i]];
+      paths = dfsMaze(maze, neighbors[i], end, visited, paths);
+      visited = visited.filter(v => !isArraysEqual(v, neighbors[i]));
+    }
+  }
+  return paths;
+}
 
 /**
  * @param maze {string[][]}
@@ -10,6 +42,19 @@ const NAME = `\n\n--- Day ${DAY}: Reindeer Maze ---`;
 function part1(maze) {
   let [start] = findChar(maze, 'S');
   let [end] = findChar(maze, 'E');
+  let visited = [start];
+
+  printMap(maze);
+  let paths = dfsMaze(maze, start, end, visited, []);
+  for (let i = 0; i < paths.length; i++) {
+    let newMap = duplicateMap(maze);
+    for (let j = 0; j < paths[i].length; j++) {
+      putChrInMap(newMap, paths[i][j], '*');
+    }
+    printMap(newMap);
+  }
+  console.log(paths);
+
 
   return maze.length;
 }
@@ -33,6 +78,15 @@ function main(input, part) {
 }
 
 let example = `
+#######
+#...#E#
+#.#.#.#
+#.#...#
+#.#.#.#
+#S#...#
+#######
+`;
+let example1 = `
 ###############
 #.......#....E#
 #.#.###.#.###.#
